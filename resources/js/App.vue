23d27f71d203f69892acd4e3fe1378abfe48f1e6
Suresh Ramani <ramani.suresh41@gmail.com>
2022-07-13 12:01:33 +0530

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,3 @@
+<template>
+    How To Install Vue 3 in Laravel 9 with Vite - TechvBlogs
+</template>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>How To Install Vue 3 in Laravel 9 with Vite</title>
+
+	@vite('resources/css/app.css')
+</head>
+<body>
+	<div id="app"></div>
+
+	@vite('resources/js/app.js')
+</body>
+</html>
